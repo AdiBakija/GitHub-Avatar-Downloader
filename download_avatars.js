964b84967628d679dca3ca8e45bece50507d75d8
Adi <adibakija35@hotmail.com>
2018-10-01 23:25:13 +0000

    Setup getRepoContributors function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,27 @@
 var request = require('request');
 
-console.log('Welcome to the GitHub Avatar Downloader!');
+function getRepoContributors(repoOwner, repoName, cb) {
+  // request.get('https://api.github.com/repos/jquery/jquery/contributors')
+  //      .on('error', function (err) {
+  //        throw err;
+  //      })
+
+
+  //      .on('response', function (response) {
+  //        console.log('Downloading Image');
+  //        console.log('Response Status Code: ', response.statusCode);
+  //        console.log('Response Status Message: ', response.statusMessage);
+  //        console.log('Content Type: ', response.headers['content-type']);
+  //      })
+
+  //      .on('end', function(response) {
+  //       console.log("Image Downloaded")
+  //      })
+
+  //      .pipe(fs.createWriteStream('./future.jpg'));
+}
+
+getRepoContributors("jquery", "jquery", function(err, result) {
+  console.log("Errors:", err);
+  console.log("Result:", result);
+});
