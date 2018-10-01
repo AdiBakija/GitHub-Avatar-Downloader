d1efc8484992343e58e3d26e739351465ad0fe63
Adi <adibakija35@hotmail.com>
2018-10-01 23:42:09 +0000

    Created secret token and required within downloads. Added secret token to gitignore

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,17 @@
 var request = require('request');
+var secrets = require('./secrets.js')
 
 function getRepoContributors(repoOwner, repoName, cb) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(options, function(err, res, body) {
+    cb(err, body);
+  });
   // request.get('https://api.github.com/repos/jquery/jquery/contributors')
   //      .on('error', function (err) {
   //        throw err;
